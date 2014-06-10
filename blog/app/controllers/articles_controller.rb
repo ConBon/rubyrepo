090aac0675e5939da3cae57b3747d1afde746633
Conal McLaughlin <conalsmclaughlin@gmail.com>
2014-06-10 18:06:40 +0100

    Finishing off tutorial

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,9 +1,34 @@
 class ArticlesController < ApplicationController
+
+http_basic_authenticate_with name: "conal", password: "roobie",
+			except: [:index, :show]
+
 def new
 end
 
 def index
   @articles = Article.all
+end
+
+def destroy
+  @article = Article.find(params[:id])
+  @article.destroy
+
+  redirect_to articles_path
+end
+
+def update
+  @article = Article.find(params[:id])
+
+  if @article.update(article_params)
+    redirect_to @article
+  else
+    render 'edit'
+  end
+end
+
+def edit
+  @article = Article.find(params[:id])
 end
 
 def show
@@ -13,8 +38,11 @@ end
 def create
   @article = Article.new(article_params)
 
-  @article.save
-  redirect_to @article
+  if @article.save
+    redirect_to @article
+  else
+    render 'new'
+  end
 end
 
 private
